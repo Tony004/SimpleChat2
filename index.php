e26e8d6d3fe9d01fb38a810bac88949568e4a6f4
Tony <tonyvclcsa@gmail.com>
2019-08-10 20:02:02 +0300

    ChatCommit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Task 1</title>
+</head>
+<body>
+        <div id="chat">
+            <div id="chatHead">Шапка чата</div>
+            <main>
+                <aside>
+                    <p>User001</p><p>User002</p><p>User003</p><p>User004</p>
+                    <p>User005</p><p>User006</p><p>User007</p><p>User008</p>
+                    <p>User009</p><p>User010</p><p>User011</p><p>User012</p>
+                    <p>User013</p><p>User014</p><p>User015</p><p>User016</p>
+                    <p>User017</p><p>User018</p><p>User019</p><p>User020</p>
+                    <p>User021</p><p>User022</p><p>User023</p><p>User024</p>
+                </aside>
+                <div id="chatText">
+                    <div>
+                        <h3>12:00 <span>User001</span></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet commodo nunc, vitae cursus dolor. Donec sagittis venenatis libero, sed vestibulum mi sodales et. Duis placerat tortor in fermentum lacinia. Cras quis magna at lorem dictum pulvinar. Morbi quis ex et ipsum tristique sagittis eu a ligula. Vivamus commodo interdum turpis, sit amet pretium est elementum eu.</p>
+                    </div>
+                    <div>
+                        <h3>13:00 <span>User002</span></h3>
+                        <p>Sed ac metus ac diam suscipit varius. Vestibulum non porttitor ante. Donec egestas quam vitae ultricies malesuada. Donec at commodo ex. Proin euismod ex eget quam malesuada accumsan. Nullam porta in elit sit amet ullamcorper. Vivamus neque ex, porttitor vitae elit sed,</p>
+                    </div>
+                    <div>
+                        <h3>14:00 <span>User003</span></h3>
+                        <p>Sed malesuada interdum porta. In faucibus volutpat diam, a laoreet ligula vestibulum eget. Proin ac facilisis dui. Aliquam ultricies maximus eros, ut hendrerit purus volutpat et. Proin hendrerit libero mattis iaculis cursus. Nulla lectus orci, congue at dui a, luctus sodales tellus. Ut nec sapien eget dui pellentesque porttitor.</p>
+                    </div>
+                    <div>
+                        <h3>15:00 <span>User004</span></h3>
+                        <p>Mauris egestas commodo nisl, eu luctus libero pretium vel. Vivamus non feugiat odio. Nullam feugiat nibh porta nisl mattis vestibulum. Nullam at facilisis turpis. Curabitur dignissim hendrerit odio, in congue ex sollicitudin id. Fusce ante magna, ultrices at sem sed, rhoncus elementum dui. Maecenas molestie risus ex. Praesent at porttitor urna. Aenean molestie nisi non velit feugiat egestas. Nam tincidunt mauris sed risus egestas interdum. Integer tristique magna ac ipsum elementum dapibus eu eu libero. Duis mollis lobortis erat sit amet rutrum. Sed in rhoncus ipsum, at fringilla orci. Maecenas vel ex non ante pharetra euismod sit amet eu ligula. Proin nec euismod ligula.</p>
+                    </div>
+                    <div>
+                        <h3>16:00 <span>User005</span></h3>
+                        <p>Sed venenatis tellus eget tortor faucibus lacinia. Cras vitae sapien non tellus dictum fringilla. Integer non faucibus tellus. Praesent sed vestibulum felis. Proin odio dolor, sodales eu est at, tempus ornare mauris. Integer tempus porttitor neque quis malesuada. Nullam metus nulla, facilisis id tortor id, placerat malesuada mi. Aliquam porta scelerisque purus quis sodales. Sed aliquet massa nunc, et suscipit lectus mattis ac.</p>
+                    </div>
+                </div>
+            </main>
+            <div class="clearfix"></div>
+            <div id="chatMsg">
+                <div id="message">
+                    <textarea name="name" rows="8" cols="80" maxlength="500" placeholder="Напишите сообщение"></textarea>
+                </div>
+                <div id="buttons">
+                    <input type="submit" value="Отправить">
+                    <input type="submit" value="Не отправлять">
+                </div>
+            </div>
+        </div>
+</body>
+</html>
